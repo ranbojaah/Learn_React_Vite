03df3c3726bbe8c8fe8742b01cec20ca43e5a4a8
ranbojah <cp.zf.salman@gmail.com>
2024-02-19 14:15:30 +0700

    Dynamic Routing

--- src/service/product.service.js
+++ src/service/product.service.js
@@ -10,3 +10,14 @@ export const getProdcuts = (callback) => {
       console.log(err)
     })
 }
+
+export const getDetailProdcut = (id, callback) => {
+  axios
+    .get(`https://fakestoreapi.com/products/${id}`)
+    .then((res) => {
+      callback(res.data)
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+}
